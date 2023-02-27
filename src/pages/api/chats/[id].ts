31b1bf691bc321eabf5d932a5fd18808b51965dc
Cyrille Derche <c.derche@me.com>
2023-02-27 18:34:22 +0100

    rm ChatPhoto

--- src/pages/api/chats/[id].ts
+++ src/pages/api/chats/[id].ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Bot } from "grammy";
-import { Chat, ChatPhoto } from 'grammy/out/types';
+import { Chat } from 'grammy/out/types';
 
 interface Data {
     title: string
